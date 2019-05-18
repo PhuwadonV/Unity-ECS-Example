2d39cbd04a7682fdcd0b32403cf934a97e92848f
Phuwadon Verasertniyom <phuwadonpeak@gmail.com>
2019-05-18 21:12:59 +0700

    add ecs example

---
+++ Assets/4_ECS/Scripts/RotationSpeed.cs
@@ -0,0 +1,6 @@
+﻿using Unity.Entities;
+
+struct RotationSpeed : IComponentData
+{
+    public float speed;
+}
---
+++ Assets/4_ECS/Scripts/RotationSpeedProxy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+[AddComponentMenu("ECS/RotationSpeed")]
+[RequireComponent(typeof(ConvertToEntity))]
+[RequiresEntityConversion]
+public class RotationSpeedProxy : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public float speed;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new RotationSpeed { speed = speed });
+    }
+}
