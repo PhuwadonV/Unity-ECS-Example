e29d7f052f4d6332dc347c16af12c9a757fda0af
Phuwadon Verasertniyom <phuwadonpeak@gmail.com>
2019-05-18 22:31:40 +0700

    update ecs example

--- Assets/4_ECS/Scripts/RotationSpeedSystem.cs
+++ Assets/4_ECS/Scripts/RotationSpeedSystem.cs
@@ -7,6 +7,7 @@ using Unity.Transforms;
 class RotationSpeedSystem : ComponentSystem
 {
     private bool checkScene = false;
+
     protected override void OnUpdate()
     {
         if (!checkScene && !SceneManager.GetActiveScene().name.Equals("ECS"))
@@ -16,11 +17,14 @@ class RotationSpeedSystem : ComponentSystem
         }
         else
         {
-            Entities.ForEach((ref Rotation rotation, ref RotationSpeed rotationSpeed) =>
-            {
-                rotation.Value = math.mul(math.normalize(rotation.Value),
-                    quaternion.AxisAngle(math.up(), rotationSpeed.speed * Time.deltaTime));
-            });
+            OnUpdateWithCheckScene();
         }
+    }
+
+    private void OnUpdateWithCheckScene()
+    {
+        Entities.ForEach((ref Rotation rotation, ref RotationSpeed rotationSpeed) =>
+            rotation.Value = math.mul(math.normalize(rotation.Value), quaternion.AxisAngle(math.up(), rotationSpeed.speed * Time.deltaTime))
+        );
     }
 }
