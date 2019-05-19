a43da350eeb3a969599b222b90ae505566f83526
Phuwadon Verasertniyom <phuwadonpeak@gmail.com>
2019-05-19 14:59:29 +0700

    add job chunk example

---
+++ Assets/6_JobChunk/Scripts/RotationSpeedJobChunkSystem.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+[BurstCompile]
+struct RotationSpeedJobChunk : IJobChunk
+{
+    public float deltaTime;
+    public ArchetypeChunkComponentType<Rotation> rotationType;
+    [ReadOnly] public ArchetypeChunkComponentType<RotationSpeed> rotationSpeedType;
+
+    public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+    {
+        var chunkRotations = chunk.GetNativeArray(rotationType);
+        var chunkRotationSpeeds = chunk.GetNativeArray(rotationSpeedType);
+
+        for (var i = 0; i < chunk.Count; i++)
+        {
+            var rotation = chunkRotations[i];
+            var rotationSpeed = chunkRotationSpeeds[i];
+
+            chunkRotations[i] = new Rotation
+            {
+                Value = math.mul(math.normalize(rotation.Value), quaternion.AxisAngle(new float3(0, 0, 1), rotationSpeed.speed * deltaTime))
+            };
+        }
+    }
+}
+
+class RotationSpeedJobChunkSystem : JobComponentSystem
+{
+    private EntityQuery entityQuery;
+
+    protected override void OnCreate()
+    {
+        if (!SceneManager.GetActiveScene().name.Equals("JobChunk"))
+        {
+            Enabled = false;
+        }
+        else
+        {
+            entityQuery = GetEntityQuery(typeof(Rotation), ComponentType.ReadOnly<RotationSpeed>());
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var rotationType = GetArchetypeChunkComponentType<Rotation>(false);
+        var rotationSpeedType = GetArchetypeChunkComponentType<RotationSpeed>(true);
+
+        var job = new RotationSpeedJobChunk()
+        {
+            rotationType = rotationType,
+            rotationSpeedType = rotationSpeedType,
+            deltaTime = Time.deltaTime
+        };
+
+        return job.Schedule(entityQuery, inputDependencies);
+    }
+}
